da0ccdc61ff59abbf1c3bef9e8912592b5574c71
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-11-20 11:23:43 +0100

    implement auth but not working yet

--- views/Signup.jsx
+++ views/Signup.jsx
@@ -1,10 +1,24 @@
 const React = require("react");
 const Layout = require("./Layout");
 
-function Signup() {
+function Signup(props) {
   return (
     <Layout>
-      <h1>hleoo</h1>
+      <form id="form" action="/auth/signup" method="POST">
+        <label>Username</label>
+        <br />
+        <input type="text" name="username" placeholder="Your username" />
+
+        <label>Password</label>
+        <br />
+        <input type="password" name="password" />
+
+        <button type="submit">Create account</button>
+
+        {props.errorMessage ? (
+          <div className="error-message"> {props.errorMessage} </div>
+        ) : null}
+      </form>
     </Layout>
   );
 }
